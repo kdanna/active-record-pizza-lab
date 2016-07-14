f1d8e15eeda83b559edbb91324ae07965208f25f
kdanna <kayce.danna@gmail.com>
2016-07-14 16:28:19 -0600

    finished lab

--- starter-code/pizza_shop/app.rb
+++ starter-code/pizza_shop/app.rb
@@ -1,2 +1,42 @@
 class PizzaShop < Sinatra::Base
+
+require 'json'
+
+	#INDEX get all pizza
+	get '/pizza' do 
+		Pizza.all.to_json
+	end
+
+	#SHOW a pizza
+	get '/pizza/:id' do
+		Pizza.find(params[:id]).to_json
+
+	end
+
+	#CREATE a pizza. 
+
+	post '/pizza' do
+		@make_pizza = JSON.parse(request.body.read)
+		@new_pizza = Pizza.new(@make_pizza)
+		@new_pizza.save
+
+	end
+
+
+	#UPDATE a pizza
+
+	put '/pizza/:id' do
+		things = JSON.parse(request.body.read)
+		@zaa = Pizza.find(params[:id])
+		@zaa.update_attributes(things)
+	end
+
+
+	#DELETE a pizza
+	delete '/pizza/:id' do
+		@zaa = Pizza.find(params[:id])
+		@zaa.destroy
+	end
+
+
 end
